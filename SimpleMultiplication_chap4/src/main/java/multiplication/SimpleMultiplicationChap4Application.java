07f9fe37b970740f736b7872893453a72fa56717
Long Nguyen <nguyenhlongvn@gmail.com>
2020-03-14 15:45:31 -0500

    Adding simple multiplication project

---
+++ SimpleMultiplication_chap4/src/main/java/multiplication/SimpleMultiplicationChap4Application.java
@@ -0,0 +1,13 @@
+package multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMultiplicationChap4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMultiplicationChap4Application.class, args);
+	}
+
+}
---
+++ SimpleMultiplication_chap4/src/test/java/multiplication/SimpleMultiplicationChap4ApplicationTests.java
@@ -0,0 +1,13 @@
+package multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMultiplicationChap4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

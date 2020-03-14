f4bef270524ebfccbde0e3111a31d41fc2e6be51
Long Nguyen <nguyenhlongvn@gmail.com>
2020-03-14 15:46:03 -0500

    Adding gamification project

---
+++ gamification-chap4/src/main/java/multiplication/GamificationChap4Application.java
@@ -0,0 +1,13 @@
+package multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificationChap4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamificationChap4Application.class, args);
+	}
+
+}
